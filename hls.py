1f2516791302e2e7f92a16c169690bd35c71049f
Martyn Jarvis <martynjarvis@gmail.com>
2014-05-08 21:12:31 +0100

    masterplaylists are now parsed, values are autmatically casted

--- hls.py
+++ hls.py
@@ -3,18 +3,75 @@ class MasterPlaylist():
     pass
 
 class MediaPlaylist():
-    pass
+    def __init__(self,name,attributes):
+        self.name = name
+        for k in attributes:
+            setattr(self,k,attributes[k])
 
 class MediaFragment():
-    def __init__(self,name,duration):
+    def __init__(self,name,attributes):
         self.name = name
-        self.duration = duration
+        self.duration = attributes # only attrib??
+
+def myBool(a):
+    if a.strip().lower()=='no':
+        return False
+    elif a.strip().lower()=='yes':
+        return True
+    raise ValueError
+
+def myDict(a):
+    a = a.split(',')
+    dct = {}
+    for b in a:
+        key,val = b.split('=')
+        key = attrName(key)
+        dct[key] = myCast(val)
+    return dct
+
+def attrName(key):
+    return key.replace('#EXT-X-','').replace('-','_').lower()
+
+def myList(a):
+    a = a.split(',')
+    if len(a)>1:
+        return [myCast(x) for x in a]
+    else:
+        raise ValueError
+
+def myCast(val):
+    # intelligent casting ish
+    try:
+        return int(val)
+    except ValueError:
+        pass
+
+    try:
+        return float(val)
+    except ValueError:
+        pass
+
+    try:
+        return myBool(val)
+    except ValueError:
+        pass
+
+    try:
+        return myDict(val)
+    except ValueError:
+        pass
+
+    try:
+        return myList(val)
+    except ValueError:
+        pass
+
+    return val
 
 
 class Player():
-    playlists=[]
-    queue = [] # download queue
-    durations = {}
+    playlists=None
+    queue = None
 
     def play(self, url, quality=None):
         # grab Master Manifest
@@ -34,13 +91,28 @@ class Player():
 
 
     def parse(self,manifest):
+
+        # remember old playlists
+        oldPlaylists = self.playlists
+        self.playlists = None
+
         lines = manifest.split('\n')
         for i,line in enumerate(lines):
             if line.startswith('#'): 
+                if 'EXT-X-STREAM-INF' in line: # media playlist special case
+                    if self.playlists is None:
+                        self.playlists = [] # forget old playlists
+                    key,val = line.split(':')
+                    attr = myCast(val)
+                    name = lines[i+1].rstrip() # next line
+                    self.playlists.append(MediaPlaylist(name,attr))
+
                 if 'EXTINF' in line: # fragment special case
-                    key,vals = line.split(':')
-                    vals = vals.split(',')
-                    duration = float(vals[0])
+                    if self.queue is None:
+                        self.queue = []
+                    key,val = line.split(':')
+                    attr = myCast(val)
+                    duration = attr[0]
                     name = lines[i+1].rstrip() # next line
                     if name not in [x.name for x in self.queue]: 
                         self.queue.append(MediaFragment(name,duration))
@@ -51,18 +123,15 @@ class Player():
                     except ValueError:
                         key = line
                         val = True
-                    key = key.replace('#EXT-X-','').replace('-','_').lower()
+                    key = attrName(key)
                     
-                    # intelligent casting ish
-                    try:
-                        val = int(val)
-                    except ValueError:
-                        try:
-                            val = float(val)
-                        except ValueError:
-                            pass
+                    val = myCast(val)
 
                     setattr(self,key,val)
+
+        # playlists weren't updated so keep old playlists
+        if self.playlists == None:
+            self.playlists = oldPlaylists 
 
         return 
 
--- test/test.py
+++ test/test.py
@@ -1,5 +1,5 @@
 import unittest
-import ..hls
+import hlslocust.hls as hls
 
 # examples
 # index.m3u8  NTV-Public-IPS.m3u8  public_200.m3u8  public_400.m3u8
@@ -12,10 +12,30 @@ class TddMasterPlaylist(unittest.TestCase):
             self.hls_player.parse(f.read())
 
     def test_playlists(self):
-        self.assertEqual(self.hls_player.playlists, 
+        playlists = [x.name for x in self.hls_player.playlists]
+        self.assertEqual(playlists, 
             ['http://public.infozen.cshls.lldns.net/infozen/public/public/public_200.m3u8',
              'http://public.infozen.cshls.lldns.net/infozen/public/public/public_400.m3u8',
              'http://public.infozen.cshls.lldns.net/infozen/public/public/public_200.m3u8'])
+
+    def test_playlist_bandwidths(self):
+        bandwidths = [x.bandwidth for x in self.hls_player.playlists]
+        self.assertEqual(bandwidths, [1000000, 400000, 200000])
+
+    def test_playlist_id(self):
+        program_ids = [x.program_id for x in self.hls_player.playlists]
+        self.assertEqual(program_ids, [1127167744, 1127167744, 1127167744])
+
+    def test_duplicate_playlists(self):
+        with open('example/NTV-Public-IPS.m3u8') as f:
+            self.hls_player.parse(f.read())
+        self.assertEqual(len(self.hls_player.playlists),3)
+
+    def test_playlists_are_remembered(self):
+        with open('example/public_200.m3u8') as f:
+            self.hls_player.parse(f.read())
+        self.assertEqual(len(self.hls_player.playlists),3)
+
 
 class TddMediaPlaylist(unittest.TestCase):
     def setUp(self):
@@ -43,7 +63,12 @@ class TddMediaPlaylist(unittest.TestCase):
         self.assertEqual(self.hls_player.media_sequence, 32458)
         self.assertEqual(self.hls_player.allow_cache, False)
         self.assertEqual(self.hls_player.version, 2)
-        self.assertEqual(self.hls_player.targetduration, 3)
-                
+
+    def test_duplicate_fragments(self):
+        with open('example/public_200.m3u8') as f:
+            self.hls_player.parse(f.read())
+        self.assertEqual(len(self.hls_player.queue),8)
+
+
 if __name__ == '__main__':
     unittest.main()
