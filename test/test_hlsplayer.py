26c5b017499cbf9a56c4efed2f057a067a0ce96d
Martyn Jarvis <martynjarvis@gmail.com>
2014-08-28 09:17:01 +0100

    port as global var

--- test/test_hlsplayer.py
+++ test/test_hlsplayer.py
@@ -10,6 +10,9 @@ import hlslocust.hlsplayer as hlsplayer
 # allow sockets to be reused when we rerun tests
 SocketServer.TCPServer.allow_reuse_address = True
 
+PORT = 8000
+HOST = "http://localhost:{p}".format(p=PORT)
+
 def side_effect():
     side_effect.fake_time+=0.2
     return side_effect.fake_time
@@ -34,7 +37,9 @@ class TddPlay(unittest.TestCase):
     @patch('time.time', new=time_mock)
     @patch('gevent.sleep', return_value=None)
     def test_live_play(self, patched_sleep):
-        buffer_time, play_time = self.hls_player.play(url='http://localhost:8000/live-example/NTV-Public-IPS.m3u8', duration=22)
+        buffer_time, play_time = self.hls_player.play(
+                    url='{h}/live-example/NTV-Public-IPS.m3u8'.format(h=HOST),
+                    duration=22)
         self.assertEqual(buffer_time,24.0)  # 'duration' of 'downloaded' files 
         self.assertGreaterEqual(play_time,22.0)  # duration of played files
         self.assertLess(play_time,23.0) 
@@ -42,14 +47,14 @@ class TddPlay(unittest.TestCase):
     @patch('time.time', new=time_mock)
     @patch('gevent.sleep', return_value=None)
     def test_vod_play(self, patched_sleep):
-        buffer_time, play_time = self.hls_player.play(url='http://localhost:8000/vod-example/index.m3u8')
+        buffer_time, play_time = self.hls_player.play(
+                    url='{h}/vod-example/index.m3u8'.format(h=HOST))
         self.assertEqual(buffer_time,141.0) 
         self.assertGreaterEqual(play_time,141.0) 
         self.assertLess(play_time,142.0) 
 
 class WebServer(threading.Thread):
     def __init__(self):
-        PORT = 8000
         Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
         self.httpd = SocketServer.TCPServer(("", PORT), Handler)
         threading.Thread.__init__(self)
